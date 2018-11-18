c62bae33aac62bb60da42aea0dce3653912b9608
Ryan Strauss <rystrauss@davidson.edu>
2018-11-17 20:24:32 -0500

    Fiddle with weighted_kmeans

--- adavan/ckmeans/weighted_kmeans.py
+++ adavan/ckmeans/weighted_kmeans.py
@@ -1,3 +1,8 @@
+"""Weighted kmeans clustering with size constraints.
+
+This implementation comes from https://github.com/Behrouz-Babaki/MinSizeKmeans.
+"""
+
 import random
 
 import numpy as np
@@ -18,10 +23,11 @@ class SubProblem(object):
         self.max_weight = max_weight
         self.n = len(data)
         self.k = len(centroids)
+        self.y = None
+        self.status = None
+        self.model = self._create_model()
 
-        self.create_model()
-
-    def create_model(self):
+    def _create_model(self):
         def distances(assignment):
             return l2_distance(self.data[assignment[0]], self.centroids[assignment[1]])
 
@@ -35,24 +41,26 @@ class SubProblem(object):
                                        cat=pulp.LpInteger)
 
         # create the model
-        self.model = pulp.LpProblem("Model for assignment subproblem", pulp.LpMinimize)
+        model = pulp.LpProblem("Model for assignment subproblem", pulp.LpMinimize)
 
         # objective function
-        self.model += pulp.lpSum(
+        model += pulp.lpSum(
             [distances(assignment) * self.weights[assignment[0]] * self.y[assignment] for assignment in
              assignments]), 'Objective Function - sum weighted squared distances to assigned centroid'
         # this is also weighted, otherwise the weighted centroid computation don't make sense.
 
         # constraints on the total weights of clusters
         for j in range(self.k):
-            self.model += pulp.lpSum([self.weights[i] * self.y[(i, j)] for i in
-                                      range(self.n)]) >= self.min_weight, "minimum weight for cluster {}".format(j)
-            self.model += pulp.lpSum([self.weights[i] * self.y[(i, j)] for i in
-                                      range(self.n)]) <= self.max_weight, "maximum weight for cluster {}".format(j)
+            model += pulp.lpSum([self.weights[i] * self.y[(i, j)] for i in
+                                 range(self.n)]) >= self.min_weight, "minimum weight for cluster {}".format(j)
+            model += pulp.lpSum([self.weights[i] * self.y[(i, j)] for i in
+                                 range(self.n)]) <= self.max_weight, "maximum weight for cluster {}".format(j)
 
         # make sure each point is assigned at least once, and only once
         for i in range(self.n):
-            self.model += pulp.lpSum([self.y[(i, j)] for j in range(self.k)]) == 1, "must assign point {}".format(i)
+            model += pulp.lpSum([self.y[(i, j)] for j in range(self.k)]) == 1, "must assign point {}".format(i)
+
+        return model
 
     def solve(self):
         self.status = self.model.solve()
@@ -96,9 +104,6 @@ def compute_centers(clusters, dataset, weights=None):
 
 
 def initialize_centers(dataset, k):
-    """
-    sample k random datapoints as starting centers
-    """
     ids = list(range(len(dataset)))
     random.shuffle(ids)
     return [dataset[id] for id in ids[:k]]
@@ -131,25 +136,6 @@ def minsize_kmeans_weighted(data, k=3, weights=None, min_weight=0, max_weight=No
     return clusters, centers
 
 
-def read_data(datafile):
-    data = []
-    with open(datafile, 'r') as f:
-        for line in f:
-            line = line.strip()
-            if line != '':
-                d = [float(i) for i in line.split()]
-                data.append(d)
-    return data
-
-
-def read_weights(weightfile):
-    weights = []
-    with open(weightfile, 'r') as f:
-        for line in f:
-            weights += [float(i) for i in line.strip().split()]
-    return weights
-
-
 def cluster_quality(cluster):
     if len(cluster) == 0:
         return 0.0
@@ -171,22 +157,18 @@ def compute_quality(data, cluster_indices):
     return sum(cluster_quality(c) for c in clusters.values())
 
 
-if __name__ == '__main__':
-    data = np.array([[1, 1], [2, 2], [4, 2], [4, 4], [5, 5], [5, 2]])
+def cluster(data, k=3, min_weight=0, max_weight=None, weights=None, max_iter=1000):
+    if weights is None:
+        weights = np.ones((data.shape[0],), dtype=np.uint8)
     best = None
     best_clusters = None
-    for i in range(1000):
-        clusters, centers = minsize_kmeans_weighted(data, 3, weights=[1, 1, 1, 1, 1, 1])
+    for i in range(max_iter):
+        clusters, centers = minsize_kmeans_weighted(data, k=k, min_weight=min_weight, max_weight=max_weight,
+                                                    weights=weights)
         if clusters:
             quality = compute_quality(data, clusters)
             if not best or (quality < best):
                 best = quality
                 best_clusters = clusters
 
-    if best:
-        print('cluster assignments:')
-        for i in range(len(clusters)):
-            print('%d: %d' % (i, clusters[i]))
-        print('sum of squared distances: %.4f' % (best))
-    else:
-        print('no clustering found')
+    return best_clusters
